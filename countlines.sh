b640737b38a534899e2662356f98f089f9d46898
Maria Pau <mariapau@MacBook-Pro-de-Maria-3.local>
2025-11-02 18:17:10 +0100

    Initial commit, upload only countlines.sh file

---
+++ countlines.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+filename=$1
+n=$(wc -l < "$filename")
+
+if [[ ! -f "$filename" ]]; then
+	echo "Error: $filename does not exist :("
+	exit 2
+fi
+
+if [[ "$n" -eq 0 ]]; then
+	echo " "$filename" has no lines"
+elif [[ "$n" -eq 1 ]]; then
+	echo " "$filename" has 1 line"
+else 
+	echo " "$filename" has more than 1 line"
+fi
